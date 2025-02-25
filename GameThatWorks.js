805da84f55b9a1233bcef7dcfe6dfc3cec326e94
MrBenBrittonHVHS <106791155+MrBenBrittonHVHS@users.noreply.github.com>
2025-02-25 00:16:54 +0000

    Base version complete

--- GameThatWorks.js
+++ GameThatWorks.js
@@ -14,6 +14,8 @@ const COINSIZE = 10;
 const COIN_TIMEOUT = 2000;
 var coin;
 
+var gameState = "play";
+
 function setup() {
 	console.log("setup: ");
 
@@ -21,8 +23,11 @@ function setup() {
 	player = new Sprite(100, 100, PLAYERSIZE, PLAYERSIZE);
 	player.color = 'green';
 
-    createCoin();
-	player.collides(coin, getPoint);
+	coins = new Group();
+
+    coins.add(createCoin());
+
+	player.collides(coins, getPoint);
     function getPoint(collider1, collider2) {
 		// Delete the coin which was hit
 		collider2.remove();
@@ -35,25 +40,58 @@ function setup() {
 // draw()
 /*******************************************************/
 function draw() {
-	background('cyan');
 
+	if (gameState == "play"){
+		runGame();
+	}else if (gameState == "lose"){
+		loseScreen();
+	}
+
+}
+function runGame(){
+	
+	background('cyan');
+	if (random(0,1000)<20){
+		coins.add(createCoin());
+	}
     movePlayer();
-    checkCoinTime()
+	for (var i = 0; i < coins.length; i++){
+		// Check Coin time should return true if the coin is old and needs to be deleted
+		if(checkCoinTime(coins[i])){
+			coins[i].remove();
+			gameState = "lose"
+		}
+	}
+	console.log(gameState);
     displayScore();
 }
 
-function checkCoinTime(){
+function loseScreen(){
+	background('red');
+	player.remove();
+	coins.remove();
+	fill(0, 0, 0);
+	textSize(50);
+	text("You missed a coin! ", 10,100);
+	textSize(100);
+
+	text("Score: " + score, 10,200);
+}
+
+function checkCoinTime(_coin){
     // Check if the coin has been around too long (COIN_TIMEPUT millisecomnds)
-    if (coin.spawntime + COIN_TIMEOUT < millis()){
-        coin.remove()
+    if (_coin.spawntime + COIN_TIMEOUT < millis()){
+        return(true);// Coin is old
     }
+	return(false);//Coin is young
 }
 
 function createCoin(){
     //random (0, WIDNOWHEIGHT)
-	coin = new Sprite(random(0, GAMEHEIGHT), random(0, GAMEWIDTH), COINSIZE);
+	var coin = new Sprite(random(0, GAMEHEIGHT), random(0, GAMEWIDTH), COINSIZE);
 	coin.color = 'yellow';
     coin.spawntime = millis();
+	return(coin);
 }
 
 function displayScore(){
